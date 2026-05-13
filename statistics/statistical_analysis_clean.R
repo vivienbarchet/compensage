56ce2aa398ffcb83d243c1004bf2debf38d7fae1
vivienbarchet <vivien.barchet@gmx.de>
2026-05-13 16:51:07 +0200

    new fits

--- statistics/statistical_analysis_clean.R
+++ statistics/statistical_analysis_clean.R
@@ -8,12 +8,12 @@ library(data.table)
 library(Hmisc)
 library(DescTools)
 library(tidyverse)
-library(readxl)
+library("readxl")
 library(Hmisc) 
 
-setwd("/data/pt_03114/trf_analysis_yo/code_upload/statistics/")
+
 #Load fits
-fits_sub_ac <- fread("./fits_ac_within.csv")
+fits_sub_ac <- fread("fits_ac_within_599.csv")
 
 
 
@@ -23,7 +23,7 @@ fits_sub_ac <- fits_sub_ac %>%
   rename(feat = feature_shuffle)
 
 
-fits_sub <- fread("./fits_ling_within.csv")
+fits_sub <- fread("fits_ling_within_599.csv")
 
 fits_sub <- fits_sub %>%
   select(-V1) %>%
@@ -34,10 +34,7 @@ fits_sub <- fits_sub %>%
 fits_sub <- rbind(fits_sub_ac, fits_sub)
 
 
-##Load TR fits 
-
-
-fits_sub_tr <- fread("./tr_fits_ling.csv")
+fits_sub_tr <- fread("tr_fits_ling_599.csv")
 
 fits_sub_tr <- fits_sub_tr %>%
   select(-V1) %>%
@@ -47,6 +44,7 @@ fits_sub_tr <- fits_sub_tr %>%
 
 
 fits_sub <- rbind(fits_sub, fits_sub_tr)
+
 
 
 fits_sub_wide <- fits_sub %>%
@@ -61,6 +59,7 @@ data = read.csv("./behavdat.csv", sep = ";")
 
 data <- data %>%
   mutate(subject = if_else(subject == "41", "sub41", subject))
+
 
 
 ###Create the trial numbers 
@@ -83,9 +82,6 @@ fits_sub <- fits_sub_wide %>%
 data_behav_eeg <- data_merge %>%
   rename(subject = subject) %>%
   merge(fits_sub, by = c('subject', 'sent_id'))
-
-
-
 
 
 baseline <- data %>%
@@ -113,22 +109,20 @@ data_model <- data_behav_eeg %>%
   ungroup() %>%
   mutate(frequency = scale(frequency)) 
 
-mean(baseline$meana)
 
-sd(data_model$srt_snr)
-range(data_model$srt_snr)
 
 data_model <- left_join(data_model, baseline, by = "subject")
 
 data_model <- data_model %>%
-  mutate(meana = scale(meana)) %>%
+  mutate(meana = scale(meana), 
+         srt_db = scale(srt_db)) %>%
   mutate(
     onsets_target=scale(onsets_target),
     onsets_dis = scale(onsets_dis), 
     ac_target = scale(ac_target), 
     ac_dis = scale(ac_dis), 
     word_target = scale(ling_word_target), 
-    phoneme_target= scale(ling_phone_target), 
+    # phoneme_target= scale(ling_phone_target),
     word_target_tr = scale(ling_word_target_tr)
   ) %>%
   ungroup() 
@@ -136,7 +130,7 @@ data_model <- data_model %>%
 
 #Load demographics
 age <- read_excel("./demographics/age_new.xlsx")
-hearing <- read_csv("./demographics/hearing_thresholds_oneside.csv")
+hearing <- read_csv("./demographics/hearing_thresholds.csv")
 wl <- read.csv("./demographics/wordlist_meanacc.csv")
 
 wl <- wl %>%
@@ -156,6 +150,7 @@ data_model <- data_model %>%
   mutate(thresholdsc = scale(threshold)) 
 
 
+###Subject-level analysis ---- 
 
 
 data_sub <- data_model %>%
@@ -174,12 +169,10 @@ data_sub <- data_model %>%
                    ac_target = mean(ac_target), 
                    ac_dis = mean(ac_dis), 
                    word_target = mean(word_target), 
-                   phoneme_target= mean(phoneme_target), 
+                   # phoneme_target= mean(phoneme_target), 
                    word_target_tr = mean(word_target_tr)
   )%>%
   mutate(acc_diff = (sub_acc - mean_acc_wl)/(sub_acc+mean_acc_wl))
-
-mean(data_sub$threshold)
 
 
 corm <- data_sub %>%
@@ -191,8 +184,6 @@ coeffs <- rcorr(as.matrix(corm))[[1]]
 
 
 pvalues <- rcorr(as.matrix(corm))[[3]]
-pvalues
-
 pvalues[is.na(pvalues)] <- 1
 library(corrplot)
 colnames(coeffs) <- c("Age",
@@ -204,7 +195,7 @@ rownames(coeffs) <-  c("Age", "Pure Tone Average", "Speech Reception Threshold",
 corrplot(coeffs, method = "color", type = "lower", addCoef.col = "black", tl.col = "black", diag = F)
 
 
-
+#Age groups for plotting
 rn <- quantile(data_sub$Age, probs = c(0.33, 0.66)) # quartile
 
 
@@ -214,6 +205,12 @@ data_sub <- data_sub %>%
                           Age > rn[2] ~ 'old'))
 
 
+
+
+#Residualize PTA
+lmage <- lm(threshold ~ age, data = data_sub)
+data_sub$ptaresid <- lmage$residuals
+
 data_sub <- data_sub %>%
   ungroup() %>%
   mutate(
@@ -222,27 +219,24 @@ data_sub <- data_sub %>%
     ac_target = scale(ac_target), 
     ac_dis = scale(ac_dis), 
     word_target = scale(word_target), 
-    phoneme_target= scale(phoneme_target),
-    word_target_tr = scale(word_target_tr))
+    # phoneme_target= scale(phoneme_target), 
+    word_target_tr = scale(word_target_tr), 
+    sub_acc = scale(sub_acc), 
+    ptaresid = scale(ptaresid), 
+    acc_diff = scale(acc_diff)
+  )
 
 
-#Residualize PTA
-lmage <- lm(threshold ~ age, data = data_sub)
-data_sub$ptaresid <- lmage$residuals
+##Word list analysis
+modwl <- lm(acc_diff ~ age + sub_acc + meana + ptaresid + srt_db, data = data_sub)
+summary(modwl)
 
 
 
 
-###Subject-level analysis ---- 
-#Wordlist accuracy
-t2 <- lm(acc_diff ~ age + sub_acc+ srt_db + meana + ptaresid, data =data_sub)
-summary(t2)
-
-feats = c('ac_target', 'ac_dis', 'onsets_target', 'onsets_dis', 'word_target', 'phoneme_target', 
-          'word_target_tr')
+feats = c('ac_target', 'ac_dis', 'onsets_target', 'onsets_dis', 'word_target',  'word_target_tr')
 
 for (f in feats){
-  
   #Green 
   greens <- RColorBrewer::brewer.pal(4, "Greens")[4]
   #Purple
@@ -253,24 +247,89 @@ for (f in feats){
   t <- lm(eq , 
           data = data_sub)
   summary(t)
-  print(summary(t))
   page = summary(t)$coef[2,4]
   
+  
+  paletteage = c('#98accf','#5579aa', '#3f4756')
+  moddat <- get_model_data(t, type = "eff", 
+                           terms = c("Age"))
+  
+  data_sub$ageg <- factor(data_sub$ageg, levels = c("young", "middle", "old"))
+  
+  
+  if (grepl('ac', f)){
+    lim = c(-3,4)
+    br = c(-2,0,2)
+    
+  }
+  else if (grepl('onsets', f)){
+    lim = c(-3,4)
+    br = c(-2,0,2)
+  }
+  else{
+    lim = c(-3,5)
+    br = c(-2,0,2)
   }
   
   
   
+  if (page < .05){
+    ggplot(moddat, aes(x = x, y = predicted)) + 
+      geom_line(size = 3, color = greens)+
+      theme_classic() + 
+      geom_ribbon(
+        aes(x = x, ymin = conf.low, ymax = conf.high), alpha = 0.5, color = greens, fill = greens) +
+      geom_point(data = data_sub, aes(x = Age, y =.data[[f]], color = ageg),
+                 size = 3, alpha = 0.5)+
+      theme(axis.title = element_text(size = 30), 
+            text = element_text(size = 25),
+            plot.title = element_text(hjust = 0.5)) + 
+      xlab("Age") + 
+      ylab("Encoding\nAccuracy") + 
+      ggtitle("") +
+      scale_y_continuous(breaks =br, limits = lim) + 
+      scale_x_continuous(breaks = c(20,40,60)) +
+      
+      scale_color_manual(values = paletteage, labels = c("Young", "Middle", "Old"), name = "Age") 
+    
+    sname = sprintf("../figures/%s_age_thr_599.svg", f)
+    ggsave(sname, width = 5, height = 3.5, unit = "in")}
+  else{
+    ggplot(moddat, aes(x = x, y = predicted)) + 
+      
+      theme_classic() + 
+      
+      geom_point(data = data_sub, aes(x = Age, y = .data[[f]], color = ageg),
+                 size = 3, alpha = 0.5)+
+      theme(axis.title = element_text(size = 30), 
+            text = element_text(size = 25),
+            plot.title = element_text(hjust = 0.5)) + 
+      xlab("Age") + 
+      ylab("Encoding\nAccuracy") + 
+      ggtitle("") +
+      scale_y_continuous(breaks = br, limits = lim) + 
+      
+      scale_x_continuous(breaks = c(20,40,60)) +
+      scale_color_manual(values = paletteage, labels = c("Young", "Middle", "Old"), name = "Age") 
+    
+    sname = sprintf("../figures/%s_age_thr_599.svg", f)
+    ggsave(sname, width = 5, height = 3.5, unit = "in")
+  }
+  
+  
+  
+}
 
 
 
-###Compensation analysis
+###Compensation analysis----
 
 
 t2 <- lm(sub_acc~age*word_target*onsets_dis +age*word_target*ac_dis+meana+ptaresid+srt_db, 
          data = data_sub)
+summary(t2)
 
 tab_model(t2, p.adjust ="fdr")
-
 
 greens3 <- RColorBrewer::brewer.pal(4, "Greens")[2:4]
 plot_model(t2, type = "eff",  terms = c( 'word_target', 'onsets_dis'), color = greens3, 
@@ -280,14 +339,41 @@ plot_model(t2, type = "eff",  terms = c( 'word_target', 'onsets_dis'), color = g
         text = element_text(size = 25)) + 
   ggtitle("") + 
   ylab("Mean\nAccuracy") + 
-  xlab("Linguistic\nWord Target") + 
+  xlab("Word Target") + 
   guides(fill=guide_legend(title="Onsets\nDistractor")) + 
   guides(color=guide_legend(title="Onsets\nDistractor"))
 
-#ggsave("../figures/compfig.png", dpi=700)
-sname = sprintf("../../../figures/compfig.png")
-ggsave(sname, width = 5.5, height =4.85, unit = "in", dpi = 700)
 
+
+
+
+
+#Save model outputs
+summary(t2)$coefficients
+cc<-confint(t2,method="Wald")
+ctab <- cbind(summary(t2)$coefficients,cc)
+
+modest = data.frame(ctab)
+modest = modest[,c(1,2,5,6,3,4)]
+modest = round(modest, 2)
+
+ord <- c("age", "word_target", "onsets_dis", "ac_dis", "word_target:onsets_dis",
+         "word_target:ac_dis",
+         
+         "age:word_target", "age:onsets_dis","age:ac_dis", 
+         "age:word_target:onsets_dis", "age:word_target:ac_dis", 
+         "meana", "ptaresid", "srt_db")
+
+modest$feat = rownames(modest)
+modest <- modest %>%
+  filter(feat != "(Intercept)")
+modest = modest %>%
+  slice(match(ord, feat)) %>%
+  select(-feat) %>%
+  mutate(CI = paste0("[", X2.5.., ", ", X97.5.., "]")) %>%
+  select(-X2.5.., -X97.5..)
+
+modest = modest[,c(1,2,5,3,4)]
 
 
 
@@ -296,127 +382,124 @@ ggsave(sname, width = 5.5, height =4.85, unit = "in", dpi = 700)
 data_model <- data_model %>%
   mutate(wordlen = str_length(word))
 
-
-eeg_mod <- glmer(acc ~ audibility*age + age*surprisal+
-                    audibility*surprisal+
-                    wordlen+
-                    entropy*age+
-                    db_cond+word_order+
-                    +frequency +sentence_len  + meana + srt_db +threshold +
-                    age*ac_target + age*ac_dis + 
-                    age*onsets_target+ age*onsets_dis + 
-                    age*word_target + 
-                    age*phoneme_target+
-                    (1|subject) + (1|target), 
-                  data = data_model,
-                  family = "binomial", control=glmerControl(optimizer="bobyqa",
-                                                            optCtrl=list(maxfun=2e5)))
-summary(eeg_mod) 
-
-tab_model(eeg_mod, p.adjust = "fdr" )
+data_model <- data_model %>%
+  merge(data_sub[c('subject', 'ptaresid')], by = "subject")
 
 
 
+
+eeg_mod_wd <- glmer(acc ~ audibility*age + age*surprisal+
+                      audibility*surprisal+
+                      wordlen+
+                      entropy*age+
+                      db_cond+word_order+
+                      +frequency +sentence_len  + meana + srt_db +ptaresid +
+                      age*ac_target + age*ac_dis + 
+                      age*onsets_target+ age*onsets_dis + 
+                      age*word_target + 
+                      # age*phoneme_target+
+                      
+                      (1|subject) + (1|target), 
+                    data = data_model,
+                    family = "binomial", control=glmerControl(optimizer="bobyqa",
+                                                              optCtrl=list(maxfun=2e5)))
+summary(eeg_mod_wd) 
+
+
+tab_model(eeg_mod_wd, p.adjust = "fdr" )
 
 library(DHARMa)
-#Model diagnostics
-simulationOutput <- simulateResiduals(fittedModel = eeg_mod, plot = T)
+simulationOutput <- simulateResiduals(fittedModel = eeg_mod_wd, plot = T)
 
 greens <- RColorBrewer::brewer.pal(4, "Greens")[2:5]
 
 plot_model(eeg_mod_wd, type = "pred",  terms = c('audibility', 'surprisal'), 
-         color = greens, 
-         line.size = 2) + 
-theme_classic() + 
-theme(axis.title = element_text(size = 30), 
-      text = element_text(size = 25)) + 
-ggtitle("") + 
-ylab("Predicted Accuracy") + 
-xlab("Audibility") + 
-guides(fill=guide_legend(title="Surprisal")) + 
-guides(color=guide_legend(title="Surprisal"))
+           color = greens, 
+           line.size = 2) + 
+  theme_classic() + 
+  theme(axis.title = element_text(size = 30), 
+        text = element_text(size = 25)) + 
+  ggtitle("") + 
+  ylab("Predicted Accuracy") + 
+  xlab("Audibility") + 
+  guides(fill=guide_legend(title="Surprisal")) + 
+  guides(color=guide_legend(title="Surprisal"))
 
 
-ggsave("../figures/surp_audind_yo.png", width = 4.5, height = 4.5, unit = "in", dpi = 700)
 
 
 plot_model(eeg_mod, type = "pred",  terms = c('entropy', 'age[-1.09, -0.22, 1.24]'), 
-         color = c("#98accf", "#5579aa", "#3f4756"), 
-         line.size = 2) + 
-theme_classic() + 
-theme(axis.title = element_text(size = 30), 
-      text = element_text(size = 25)) + 
-ggtitle("") + 
-ylab("Predicted Accuracy") + 
-xlab("Entropy") + 
-guides(fill=guide_legend(title="Age")) + 
-guides(color=guide_legend(title="Age"))
+           color = c("#98accf", "#5579aa", "#3f4756"), 
+           line.size = 2) + 
+  theme_classic() + 
+  theme(axis.title = element_text(size = 30), 
+        text = element_text(size = 25)) + 
+  ggtitle("") + 
+  ylab("Predicted Accuracy") + 
+  xlab("Entropy") + 
+  guides(fill=guide_legend(title="Age")) + 
+  guides(color=guide_legend(title="Age"))
 
 
-ggsave("../figures/entropy_ageint.png", width = 4, height = 4.5, unit = "in", dpi = 700 )
 
 
-plot_model(eeg_mod, type = "pred",  terms = c('onsets_dis', 'age[-1.09, -0.22, 1.24]'), color = c("#98accf", "#5579aa", "#3f4756"), 
-         line.size = 2) + 
-theme_classic() + 
-theme(axis.title = element_text(size = 30), 
-      text = element_text(size = 25)) + 
-scale_y_continuous(breaks = seq(0.7, 1, 0.1), limits = c(0.7, 0.95)) + 
-ggtitle("") + 
-ylab("") + 
-xlab("Encoding\nAccuracy") + 
-guides(fill=guide_legend(title="Age")) + 
-guides(color=guide_legend(title="Age"))
+plot_model(eeg_mod_wd, type = "pred",  terms = c('onsets_dis', 'age[-1.09, -0.22, 1.24]'), color = c("#98accf", "#5579aa", "#3f4756"), 
+           line.size = 2) + 
+  theme_classic() + 
+  theme(axis.title = element_text(size = 30), 
+        text = element_text(size = 25)) + 
+  scale_y_continuous(breaks = seq(0.7, 1, 0.1), limits = c(0.7, 0.95)) + 
+  ggtitle("") + 
+  ylab("") + 
+  xlab("Encoding\nAccuracy") + 
+  guides(fill=guide_legend(title="Age")) + 
+  guides(color=guide_legend(title="Age"))
 
-
-ggsave("../figures/onsets_dis_ageint.png", width = 4, height = 4, unit = "in" , dpi = 700)
 
 
 
 
 library(interactions)
-sim_slopes(eeg_mod, pred = onsets_dis, modx =age, johnson_neyman = TRUE, 
-         modx.values = c(-1.09, -0.22, 1.24), jnplot = TRUE)
+sim_slopes(eeg_mod_wd, pred = onsets_dis, modx =age, johnson_neyman = TRUE, 
+           modx.values = c(-1.09, -0.22, 1.24), jnplot = TRUE)
 
 
 
 
 
 library(car)
-vif(eeg_mod)
-range(vif(eeg_mod))
+vif(eeg_mod_wd)
+range(vif(eeg_mod_wd))
 
 
 
 #Save model outputs
-summary(eeg_mod)$coefficients
-cc<-confint(eeg_mod,parm="beta_",method="Wald")
-ctab <- cbind(summary(eeg_mod)$coefficients,cc)
+summary(eeg_mod_wd)$coefficients
+cc<-confint(eeg_mod_wd,parm="beta_",method="Wald")
+ctab <- cbind(summary(eeg_mod_wd)$coefficients,cc)
 
 modest = data.frame(ctab)
 modest = modest[,c(1,2,5,6,3,4)]
 modest = round(modest, 2)
 
 ord <- c("age", "audibility", "surprisal", "audibility:surprisal","frequency", "entropy","audibility:age",
-       "age:surprisal",
-       "age:entropy",
-       "ac_target","ac_dis",  "onsets_target", 'onsets_dis',
-       "age:ac_target", "age:ac_dis", "age:onsets_target", "age:onsets_dis",
-       "word_target", "phoneme_target", 
-       "age:word_target", "age:phoneme_target",
-       'threshold',
-       'db_cond','wordlen', 'word_order', 'sentence_len', 'meana', 'srt_db')
+         "age:surprisal",
+         "age:entropy",
+         "ac_target","ac_dis",  "onsets_target", 'onsets_dis',
+         "age:ac_target", "age:ac_dis", "age:onsets_target", "age:onsets_dis",
+         "word_target", "phoneme_target", 
+         "age:word_target", "age:phoneme_target",
+         'ptaresid',
+         'db_cond','wordlen', 'word_order', 'sentence_len', 'meana', 'srt_db')
 
 modest$feat = rownames(modest)
 modest <- modest %>%
-filter(feat != "(Intercept)")
+  filter(feat != "(Intercept)")
 modest = modest %>%
-slice(match(ord, feat)) %>%
-select(-feat) %>%
-mutate(CI = paste0("[", X2.5.., ", ", X97.5.., "]")) %>%
-select(-X2.5.., -X97.5..)
+  slice(match(ord, feat)) %>%
+  select(-feat) %>%
+  mutate(CI = paste0("[", X2.5.., ", ", X97.5.., "]")) %>%
+  select(-X2.5.., -X97.5..)
 
 modest = modest[,c(1,2,5,3,4)]
-
-write.csv(modest, "/data/pt_03114/figures/modeleffects_yo.csv")
 
